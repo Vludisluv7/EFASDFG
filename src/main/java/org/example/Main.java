ace0708be57d75bc9a03bad7e2f752210b9e5d07
Vludisluv <mukha165@mail.ru>
2024-01-21 13:20:48 +0300

    fynally

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+                Employee employee1 = new Employee("Иванов", 5000, "2021-01-01");
+                Employee employee2 = new Employee("Петров", 6000, "2020-12-01");
+                Manager manager = new Manager("Сидоров", 8000, "2022-03-01");
+
+                Employee[] employees = {employee1, employee2, manager};
+
+                System.out.println("Исходные данные:");
+                for (Employee employee : employees) {
+                    System.out.println(employee);
+                }
+
+                Employee.increaseSalary(employees);
+
+                System.out.println("\nПовышение зарплаты:");
+                for (Employee employee : employees) {
+                    System.out.println(employee);
+                }
+
+        }
+
+
+    }
+
